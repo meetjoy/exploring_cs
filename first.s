133c55719b33cb65339a373615c64a3decc63c33
wei <a933476@gmail.com>
2019-04-04 15:48:48 -0700

    first.s update

--- first.s
+++ first.s
@@ -1,21 +1,17 @@
-    .code16
+.code16
     .global _start
 _start:
-    mov $48, %cx
-    mov $0x0F01, %dx
-    mov $0x0001, %bx
+    mov $26, %cx
+    mov $0x0903, %dx
+    mov $0x000c, %bx
     mov $msg, %bp
     mov $0x1301, %ax
     int $0x10
-    jmp .
-msg:.ascii "Hello, World! This is my first assembly program."
+loop: jmp loop
+msg: .ascii "My first computer program!"
     .org 510
     .word 0xAA55
 
 # as first.s -o first.o
 # ld -Ttext=0x7c00 -o first.img --oformat=binary first.o
 # sudo qemu-system-i386 -drive format=raw,file=first.img
-
-
-
-
