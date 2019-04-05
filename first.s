8df564234ab320c8f964fa39b753f9759f1bd8b6
jesse-linux <a933476@gmail.com>
2019-04-05 01:57:11 -0700

    three commands, as, ld, qemu-system-x86_64 added as comments in first.s file

--- first.s
+++ first.s
@@ -1,17 +1,17 @@
 .code16
-    .global _start
+.global _start
 _start:
-    mov $26, %cx
-    mov $0x0903, %dx
-    mov $0x000c, %bx
-    mov $msg, %bp
-    mov $0x1301, %ax
-    int $0x10
+mov $26, %cx
+mov $0x0903, %dx
+mov $0x000c, %bx
+mov $msg, %bp
+mov $0x1301, %ax
+int $0x10
 loop: jmp loop
 msg: .ascii "My first computer program!"
-    .org 510
-    .word 0xAA55
+.org 510
+.word 0xAA55
+#as -o first.o first.s
+#ld -Ttext=0x7c00 -o first.img --oformat=binary first.o
+#sudo qemu-system-x86_64 -cpu max -drive format=raw,file=first.img
 
-# as first.s -o first.o
-# ld -Ttext=0x7c00 -o first.img --oformat=binary first.o
-# sudo qemu-system-i386 -drive format=raw,file=first.img
