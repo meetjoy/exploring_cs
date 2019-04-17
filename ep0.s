a2702dc351734d04f4f56ae795867decfe3e77f6
jesse <a933476@gmail.com>
2019-04-17 00:51:59 -0700

    segment register added

--- ep0.s
+++ ep0.s
@@ -19,8 +19,8 @@ _start:                 # main program body
 main:
     movb $0x0A, %ah     # set the colour attributes
     movb $67, %al       # set the code point to %al
-    movw  $SCN_SEL, %ecx#
-    movw  $scn_pos, %edx#   
+    movl SCN_SEL, %ebx  #
+    movl $scn_pos, %edx #   
     call write_screen   # call function to print 0 to screen
     jmp .
 
@@ -28,15 +28,12 @@ main:
 write_screen:           
     pushw %es           # put %es to stack
     pushl %ebx          # put %ebx to stack
-    pushl %ecx          # put %ecx to stack
     pushl %edx          # put %edx to stack
-    movl  (%ecx), %ebx  # 
     movw  %bx,%es       # set %es segment register to colour text area of video RAM
     movl  (%edx), %ebx  # 
     shll  %ebx           # time %bx by 2
     movw  %ax, %es:(%bx)# write 2 bytes to screen, 1 character displayed
     popl  %edx          # recover %edx from stack
-    popl  %ecx          # recover %ecx from stack
     popl  %ebx          # recover %ebx from stack
     popw  %es           # recover %es from stack
     ret
@@ -47,7 +44,7 @@ jump_offset : .word main
 jump_segment: .word 0x0
 
 SCN_SEL:  .word 0xb800  #! memory number where colour text starts
-scn_pos:  .word 0x0     # 2 bytes to save current screen postion
+scn_pos:  .word 0x10     # 2 bytes to save current screen postion
 .org 510                # to advance the location to 510th byte
 .word 0xAA55            # last 16 bits of first 512 bytes on disk
 
