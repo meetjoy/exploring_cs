6475bd2e989072d3c0113120283a6bcddfab5fc0
jesse <a933476@gmail.com>
2019-09-02 15:52:42 +0800

    end of 1stAss

---
+++ 1_first_assembly_program/exercise-ori/first.s
@@ -0,0 +1,13 @@
+.code16
+.global _start
+_start:
+	mov $26, %cx
+	mov $0x0903, %dx
+	mov $0x000c, %bx
+	mov $msg, %bp
+	mov $0x1301, %ax
+	int $0x10
+loop: jmp loop
+msg: .ascii "My first assembly program!"
+.org 510
+.word 0xAA55
